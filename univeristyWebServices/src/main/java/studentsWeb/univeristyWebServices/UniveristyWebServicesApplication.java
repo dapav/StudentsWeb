7b2dc79e8abf0528b2b1b53b9e3fa796e20b674c
dapav <82372150+dapav@users.noreply.github.com>
2023-02-24 10:28:25 +0200

    Add files via upload
    Initial project folder created with spring boot initializer.

---
+++ univeristyWebServices/src/main/java/studentsWeb/univeristyWebServices/UniveristyWebServicesApplication.java
@@ -0,0 +1,13 @@
+package studentsWeb.univeristyWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniveristyWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniveristyWebServicesApplication.class, args);
+	}
+
+}
---
+++ univeristyWebServices/src/test/java/studentsWeb/univeristyWebServices/UniveristyWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package studentsWeb.univeristyWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniveristyWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
